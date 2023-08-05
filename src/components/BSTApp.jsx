95f0d2f2f41bdb065fe62228e85a652e619ca5ab
Julia Brauer <julia.brauer83@gmail.com>
2023-08-04 21:17:36 -0400

    added wireframe for BSTApp

--- src/components/BSTApp.jsx
+++ src/components/BSTApp.jsx
@@ -4,7 +4,55 @@ const BSTApp = () => {
 	
 	return (
 		<div className='BSTApp'>
+			<div className='tree'>
 
+				<div className='treeLv 0'>
+					<div className='nodeLv 0'>0</div>
+				</div>
+
+				<div className='treeLv 1'>
+					<div className='nodeLv 1'>1</div>
+					<div className='nodeLv 1'>1</div>
+				</div>
+				
+				<div className='treeLv 2'>
+					<div className='nodeLv 2'>2</div>
+					<div className='nodeLv 2'>2</div>
+					<div className='nodeLv 2'>2</div>
+					<div className='nodeLv 2'>2</div>
+				</div>
+				
+				<div className='treeLv 3'>
+					<div className='nodeLv 3'>3</div>
+					<div className='nodeLv 3'>3</div>
+					<div className='nodeLv 3'>3</div>
+					<div className='nodeLv 3'>3</div>
+					<div className='nodeLv 3'>3</div>
+					<div className='nodeLv 3'>3</div>
+					<div className='nodeLv 3'>3</div>
+					<div className='nodeLv 3'>3</div>
+				</div>
+				
+				<div className='treeLv 4'>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+					<div className='nodeLv 4'>4</div>
+				</div>
+
+			</div>
 		</div>
 	)
 }
--- src/pages/BSTPage.jsx
+++ src/pages/BSTPage.jsx
@@ -5,8 +5,6 @@ import BSTApp from '../components/BSTApp'
 
 const BSTPage = () => {
 
-	const n = 20;
-
 	return (
 		<div className='BSTPage'>
 			<h1>BST</h1>
