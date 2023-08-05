b10842dabf079eb38bc17d51de21bcee35e34972
Julia Brauer <julia.brauer83@gmail.com>
2023-08-04 23:27:33 -0400

    adjusted spacing in BSTApp

--- src/components/BSTApp.jsx
+++ src/components/BSTApp.jsx
@@ -6,23 +6,23 @@ const BSTApp = () => {
 		<div className='BSTApp'>
 			<div className='tree'>
 
-				<div className='treeLv 0'>
-					<div className='nodeLv 0'>0</div>
+				<div className='treeLv lv0'>
+					<div className='nodeLv lv0'>0</div>
 				</div>
 
-				<div className='treeLv 1'>
-					<div className='nodeLv 1'>1</div>
-					<div className='nodeLv 1'>1</div>
+				<div className='treeLv lv1'>
+					<div className='nodeLv lv1'>1</div>
+					<div className='nodeLv lv1'>1</div>
 				</div>
 				
-				<div className='treeLv 2'>
+				<div className='treeLv lv2'>
 					<div className='nodeLv 2'>2</div>
 					<div className='nodeLv 2'>2</div>
 					<div className='nodeLv 2'>2</div>
 					<div className='nodeLv 2'>2</div>
 				</div>
 				
-				<div className='treeLv 3'>
+				<div className='treeLv lv3'>
 					<div className='nodeLv 3'>3</div>
 					<div className='nodeLv 3'>3</div>
 					<div className='nodeLv 3'>3</div>
@@ -33,7 +33,7 @@ const BSTApp = () => {
 					<div className='nodeLv 3'>3</div>
 				</div>
 				
-				<div className='treeLv 4'>
+				<div className='treeLv lv4'>
 					<div className='nodeLv 4'>4</div>
 					<div className='nodeLv 4'>4</div>
 					<div className='nodeLv 4'>4</div>
