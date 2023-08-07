7c87497a01d69dcf15770bf9fdc8777c2552dbe8
Julia Brauer <julia.brauer83@gmail.com>
2023-08-07 02:41:25 -0400

    added NASA Apod as background to MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,12 +7,16 @@ const MainPage = (props) => {
 	return (
 		<div className='MainPage'>
 			<h1>Main</h1>
-			<div className='mainPageContentDiv'>
+			<div className='mainPageContentDiv' 
+					style={{ backgroundImage: `url(${imgSrc})` }}>
 				<div className='mainPageAnimationDiv'>Animation 1</div>
 				<div className='mainPageAnimationDiv'>Animation 2</div>
 				<div className='mainPageAnimationDiv'>Animation 3</div>
+				<div className='mainPageAnimationDiv'>Animation 4</div>
+				<div className='mainPageAnimationDiv'>Animation 5</div>
+				<div className='mainPageAnimationDiv'>Animation 6</div>
 			</div>
-			<div className='apodImg'><img src={imgSrc} /></div>
+			{/* <div className='apodImg'><img src={imgSrc} /></div> */}
 		</div>
 		
 	)
